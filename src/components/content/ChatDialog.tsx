e25e6ee2f74224a6a1ad0a5cb812040ce9b73e98
Bendixx <Bendik.Berntsen@nav.no>
2024-09-16 14:24:36 +0200

    lagt til scrolltoview for siste melding

--- src/components/content/ChatDialog.tsx
+++ src/components/content/ChatDialog.tsx
@@ -2,6 +2,7 @@ import { VStack } from "@navikt/ds-react";
 import { Message } from "../../types/Message.ts";
 import UserQuestionBubble from "./chatbubbles/UserQuestionBubble.tsx";
 import BobAnswerBubble from "./chatbubbles/BobAnswerBubble.tsx";
+import {useEffect, useRef} from "react";
 
 interface ChatDialogProps {
   conversationId: string;
@@ -12,8 +13,16 @@ interface ChatDialogProps {
 // har svart før man kan sende ny message.
 
 function ChatDialog({ messages }: ChatDialogProps) {
+  const lastMessageRef = useRef <HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [messages]);
+
   return (
-    <VStack gap="10" width="100%">
+    <VStack gap="10" width="100%" className="pb-24">
       {
         messages.map(message => (
           message.messageRole === "human"
@@ -21,6 +30,7 @@ function ChatDialog({ messages }: ChatDialogProps) {
             : <BobAnswerBubble answer={message} />
         ))
       }
+     <div ref={lastMessageRef} />
     </VStack>
   );
 }
