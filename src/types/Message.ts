4d5c5b90efed95b15ee7885cc1bab06f89ff069f
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2024-09-16 10:12:03 +0200

    add new message types

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,9 +1,40 @@
-export enum UserType {
-  Bruker = "BRUKER",
-  ChatBob = "CHATBOB",
-}
+export type MessageType = "question" | "answer";
 
-export interface Message {
-  userType: UserType;
+export type MessageRole = "human" | "ai";
+
+export type Feedback = {
+  id: string;
+  liked: boolean;
+  createdAt: string;
+};
+
+export type Citation = {
+  id: string;
   text: string;
-}
+  article: string;
+  title: string;
+  section: string;
+  createdAt: string;
+};
+
+export type NewMessage = {
+  content: string;
+};
+
+export type Message = {
+  id: string;
+  content: string;
+  createdAt: string;
+  feedback: Feedback;
+  messageType: MessageType;
+  messageRole: MessageRole;
+  createdBy: string;
+  citations: Citation[];
+};
+
+export type Conversation = {
+  id: string;
+  title: string;
+  createdAt: string;
+  owner: string;
+};
