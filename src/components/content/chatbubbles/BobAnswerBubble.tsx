5dfa75272ec55299fd177577a7bd86bf954dc072
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2024-09-18 10:48:41 +0200

    render bob answers as markdown

--- src/components/content/chatbubbles/BobAnswerBubble.tsx
+++ src/components/content/chatbubbles/BobAnswerBubble.tsx
@@ -1,5 +1,6 @@
 import { Chat, HStack } from "@navikt/ds-react";
 
+import Markdown from "react-markdown";
 import bobhead from "../../../assets/Bob-hode-svg.svg";
 import { Message } from "../../../types/Message";
 
@@ -8,7 +9,9 @@ function BobAnswerBubble({ answer }: { answer: Message }) {
     <HStack gap="3" align="end" wrap={false} width="full">
       <img src={bobhead} alt="Bob" width="50px" className="hidehead" />
       <Chat variant="info" className="flex w-full">
-        <Chat.Bubble>{answer.content}</Chat.Bubble>
+        <Chat.Bubble>
+          <Markdown>{answer.content}</Markdown>
+        </Chat.Bubble>
       </Chat>
     </HStack>
   );
