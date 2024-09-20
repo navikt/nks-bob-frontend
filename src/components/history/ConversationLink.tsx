b79167aceb97868be4e4fa57c5ca44fe6cebbb49
Bendixx <Bendik.Berntsen@nav.no>
2024-09-20 15:00:45 +0200

    Historiekomponent ferdig stylet før brukertest

--- src/components/history/ConversationLink.tsx
+++ src/components/history/ConversationLink.tsx
@@ -1,5 +1,6 @@
 import { BodyShort, Button } from "@navikt/ds-react";
 import * as ReactRouter from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Conversation } from "../../types/Message.ts";
 
 interface ConversationLinkProps {
@@ -8,16 +9,24 @@ interface ConversationLinkProps {
 }
 
 function ConversationLink({ conversation, loading }: ConversationLinkProps) {
+  const location = useLocation();
+  const isActive = location.pathname === `/samtaler/${conversation.id}`;
+
   return (
     <Button
       size="small"
       variant="tertiary-neutral"
       as={ReactRouter.Link}
       to={`/samtaler/${conversation.id}`}
-      className="flex grow justify-start"
+      className={`flex grow justify-start ${
+        isActive ? "bg-surface-neutral-subtle" : ""
+      }`}
       loading={loading}
+      weight="regular"
     >
-      <BodyShort size="small">{conversation.title}</BodyShort>
+      <BodyShort size="small" weight={isActive ? "semibold" : "regular"}>
+        {conversation.title}
+      </BodyShort>
     </Button>
   );
 }
