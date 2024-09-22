e97647530a38202e74c2ad05a719c672f12249bd
Bendixx <Bendik.Berntsen@nav.no>
2024-09-22 12:21:11 +0200

    Endret plassering av citation card

--- src/components/content/chatbubbles/BobAnswerBubble.tsx
+++ src/components/content/chatbubbles/BobAnswerBubble.tsx
@@ -1,4 +1,4 @@
-import { Chat, HStack } from "@navikt/ds-react";
+import { Chat, HStack, VStack } from "@navikt/ds-react";
 
 import Markdown from "react-markdown";
 import Bobhead from "../../../assets/Bob-hode-svg.svg";
@@ -10,12 +10,14 @@ function BobAnswerBubble({ answer }: { answer: Message }) {
     <HStack gap="3" align="end" wrap={false} width="full">
       <img src={Bobhead} alt="Bob" width="50px" className="hidehead" />
       <Chat variant="info" className="flex w-full">
-        <Chat.Bubble>
-          <Markdown>{answer.content}</Markdown>
+        <VStack>
+          <Chat.Bubble>
+            <Markdown>{answer.content}</Markdown>
+          </Chat.Bubble>
           {answer.citations.map((citation) => (
             <BobAnswerCitation citation={citation} key={citation.id} />
           ))}
-        </Chat.Bubble>
+        </VStack>
       </Chat>
     </HStack>
   );
