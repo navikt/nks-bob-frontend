1b4a0e4a0e71dcd2cdae01b99b6c763ae82403f8
Bendixx <Bendik.Berntsen@nav.no>
2024-09-23 15:20:59 +0200

    Må teste noe mot dev. Ser ut som kilden blir tatt med som tekst i Markdown-feltet av en eller annen grunn.

--- src/components/content/chatbubbles/BobAnswerBubble.tsx
+++ src/components/content/chatbubbles/BobAnswerBubble.tsx
@@ -1,9 +1,8 @@
-import { Chat, Heading, HStack, VStack } from "@navikt/ds-react";
+import { Chat, HStack, VStack } from "@navikt/ds-react";
 
 import Markdown from "react-markdown";
 import Bobhead from "../../../assets/Bob-hode-svg.svg";
 import { Message } from "../../../types/Message";
-import BobAnswerCitation from "./BobAnswerCitation.tsx";
 
 function BobAnswerBubble({ answer }: { answer: Message }) {
   return (
@@ -16,16 +15,6 @@ function BobAnswerBubble({ answer }: { answer: Message }) {
           </Chat.Bubble>
         </Chat>
       </HStack>
-      {answer.citations?.length > 0 && (
-        <VStack gap="3" align="stretch" className="pl-16">
-          <Heading as="h5" size="xsmall">
-            Kilder
-          </Heading>
-          {answer.citations.map((citation) => (
-            <BobAnswerCitation citation={citation} key={citation.id} />
-          ))}
-        </VStack>
-      )}
     </VStack>
   );
 }
