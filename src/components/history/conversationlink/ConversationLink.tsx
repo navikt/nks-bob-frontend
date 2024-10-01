1209b7317cd430e8ee1658aa392266a10de0b9bb
Bendixx <Bendik.Berntsen@nav.no>
2024-10-01 15:05:50 +0200

    Endret lenke til path="/" for sletting av melding

--- src/components/history/conversationlink/ConversationLink.tsx
+++ src/components/history/conversationlink/ConversationLink.tsx
@@ -1,6 +1,6 @@
 import { TrashIcon } from "@navikt/aksel-icons"
 import { BodyShort } from "@navikt/ds-react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { useDeleteConversation } from "../../../api/api.ts"
 import { Conversation } from "../../../types/Message.ts"
 import "./ConversationLink.css"
@@ -12,15 +12,11 @@ interface ConversationLinkProps {
 function ConversationLink({ conversation }: ConversationLinkProps) {
   const { deleteConversation } = useDeleteConversation(conversation)
 
-  const navigate = useNavigate()
   const location = useLocation()
   const isActive = location.pathname === `/samtaler/${conversation.id}`
 
   const handleDelete = async () => {
     await deleteConversation()
-    if (location.pathname === `/samtaler/${conversation.id}`) {
-      navigate("/")
-    }
   }
 
   return (
@@ -31,13 +27,13 @@ function ConversationLink({ conversation }: ConversationLinkProps) {
       <BodyShort truncate={true} size='medium'>
         {conversation.title}
       </BodyShort>
-      <div onClick={handleDelete} className='deletebutton'>
+      <Link to='/' onClick={handleDelete} className='deletebutton'>
         <TrashIcon
           title='Slett'
           fontSize='1.3rem'
           className='hover:accent-surface-danger-hover'
         />
-      </div>
+      </Link>
     </a>
   )
 }
