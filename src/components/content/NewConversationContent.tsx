f1d1608f65868c3f5afc1a45afc18a3b8091df07
Bendixx <Bendik.Berntsen@nav.no>
2024-10-07 20:27:14 +0200

    tilbakestilt endring for test

--- src/components/content/NewConversationContent.tsx
+++ src/components/content/NewConversationContent.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useCreateConversation } from "../../api/api.ts"
-import { NewConversation, NewMessage } from "../../types/Message.ts"
+import { Message, NewConversation, NewMessage } from "../../types/Message.ts"
 import HistoryContent from "../history/HistorySidebar.tsx"
 import InputField from "../inputfield/InputField.tsx"
 import BobPlaceHolder from "./BobPlaceHolder.tsx"
+import ChatDialog from "./ChatDialog.tsx"
 import DialogWrapper from "./wrappers/DialogWrapper.tsx"
 
 function NewConversationContent() {
   const { createConversation } = useCreateConversation()
   const navigate = useNavigate()
+  const [messagePlaceholders, setMessagePlaceholders] = useState<
+    Partial<Message>[]
+  >([])
 
   function handleUserMessage(message: NewMessage) {
     const newConversation: NewConversation = {
@@ -16,6 +21,10 @@ function NewConversationContent() {
       initialMessage: { content: message.content },
     }
 
+    setMessagePlaceholders([
+      { content: message.content, messageRole: "human" },
+      { content: " ", messageRole: "ai" }, // TODO loading tekst/komponent.
+    ])
     createConversation(newConversation)
       .then((conversation) => {
         navigate(`/samtaler/${conversation.id}`)
@@ -23,6 +32,7 @@ function NewConversationContent() {
       .catch((error) => {
         // blir på en måte det samme som `rollbackOnError`
         console.error(error)
+        setMessagePlaceholders([])
       })
   }
 
@@ -30,8 +40,14 @@ function NewConversationContent() {
     <div className='contentwrapper'>
       <HistoryContent />
       <DialogWrapper>
-        <BobPlaceHolder />
-        <div className='dialogcontent pb-14'>
+        {messagePlaceholders.length === 0 && <BobPlaceHolder />}
+        {messagePlaceholders.length !== 0 && (
+          <ChatDialog
+            messages={messagePlaceholders as Message[]}
+            conversationId={"unknown"}
+          />
+        )}
+        <div className='dialogcontent pb-20'>
           <InputField onSend={handleUserMessage} />
         </div>
       </DialogWrapper>
