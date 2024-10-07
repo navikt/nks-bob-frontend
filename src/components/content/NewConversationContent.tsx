8999a09740f26649aacf6e48ef6e7efa0158af48
Bendixx <Bendik.Berntsen@nav.no>
2024-10-07 19:58:07 +0200

    ny meldingsflyt

--- src/components/content/NewConversationContent.tsx
+++ src/components/content/NewConversationContent.tsx
@@ -1,19 +1,14 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useCreateConversation } from "../../api/api.ts"
-import { Message, NewConversation, NewMessage } from "../../types/Message.ts"
+import { NewConversation, NewMessage } from "../../types/Message.ts"
 import HistoryContent from "../history/HistorySidebar.tsx"
 import InputField from "../inputfield/InputField.tsx"
 import BobPlaceHolder from "./BobPlaceHolder.tsx"
-import ChatDialog from "./ChatDialog.tsx"
 import DialogWrapper from "./wrappers/DialogWrapper.tsx"
 
 function NewConversationContent() {
   const { createConversation } = useCreateConversation()
   const navigate = useNavigate()
-  const [messagePlaceholders, setMessagePlaceholders] = useState<
-    Partial<Message>[]
-  >([])
 
   function handleUserMessage(message: NewMessage) {
     const newConversation: NewConversation = {
@@ -21,10 +16,6 @@ function NewConversationContent() {
       initialMessage: { content: message.content },
     }
 
-    setMessagePlaceholders([
-      { content: message.content, messageRole: "human" },
-      { content: " ", messageRole: "ai" }, // TODO loading tekst/komponent.
-    ])
     createConversation(newConversation)
       .then((conversation) => {
         navigate(`/samtaler/${conversation.id}`)
@@ -32,7 +23,6 @@ function NewConversationContent() {
       .catch((error) => {
         // blir på en måte det samme som `rollbackOnError`
         console.error(error)
-        setMessagePlaceholders([])
       })
   }
 
@@ -40,13 +30,7 @@ function NewConversationContent() {
     <div className='contentwrapper'>
       <HistoryContent />
       <DialogWrapper>
-        {messagePlaceholders.length === 0 && <BobPlaceHolder />}
-        {messagePlaceholders.length !== 0 && (
-          <ChatDialog
-            messages={messagePlaceholders as Message[]}
-            conversationId={"unknown"}
-          />
-        )}
+        <BobPlaceHolder />
         <div className='dialogcontent pb-14'>
           <InputField onSend={handleUserMessage} />
         </div>
