c4ba77f6beaf703ec2a9215c721b689c027b348b
Bendixx <Bendik.Berntsen@nav.no>
2024-10-08 13:47:44 +0200

    Liten feil

--- src/components/content/CreateConversationContent.tsx
+++ src/components/content/CreateConversationContent.tsx
@@ -3,6 +3,7 @@ import { useCreateConversation } from "../../api/api.ts"
 import { NewConversation, NewMessage } from "../../types/Message.ts"
 import HistoryContent from "../history/HistorySidebar.tsx"
 import InputField from "../inputfield/InputField.tsx"
+import BobPlaceHolder from "./placeholders/BobPlaceHolder.tsx"
 import DialogWrapper from "./wrappers/DialogWrapper.tsx"
 
 const CreateConversationContent = () => {
@@ -29,6 +30,7 @@ const CreateConversationContent = () => {
     <div className='contentwrapper'>
       <HistoryContent />
       <DialogWrapper>
+        <BobPlaceHolder />
         <InputField onSend={handleUserMessage} />
       </DialogWrapper>
     </div>
