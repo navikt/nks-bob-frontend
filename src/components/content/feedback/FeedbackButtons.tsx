2125289be1ed0ea7e92ec502274faebaa7a0e76e
Bendixx <Bendik.Berntsen@nav.no>
2024-10-09 13:45:58 +0200

    feedback-knapper

--- src/components/content/feedback/FeedbackButtons.tsx
+++ src/components/content/feedback/FeedbackButtons.tsx
@@ -1,22 +1,30 @@
 import { Button } from "@navikt/ds-react"
+import { useState } from "react"
 import {
   ThumbDownOutline,
   ThumbUpOutline,
 } from "../../../assets/icons/Thumbs.tsx"
 
 function FeedbackButtons() {
+  const [showThumbsUp, setShowThumbsUp] = useState(true)
+
   return (
     <div className='flex justify-end'>
-      <Button
-        variant='tertiary-neutral'
-        size='small'
-        icon={<ThumbUpOutline />}
-      ></Button>
-      <Button
-        variant='tertiary-neutral'
-        size='small'
-        icon={<ThumbDownOutline />}
-      ></Button>
+      {showThumbsUp ? (
+        <Button
+          variant='tertiary-neutral'
+          size='small'
+          icon={<ThumbUpOutline />}
+          onClick={() => setShowThumbsUp(false)}
+        ></Button>
+      ) : (
+        <Button
+          variant='tertiary-neutral'
+          size='small'
+          icon={<ThumbDownOutline />}
+          onClick={() => setShowThumbsUp(true)}
+        ></Button>
+      )}
     </div>
   )
 }
