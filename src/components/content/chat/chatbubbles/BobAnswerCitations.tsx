d6453eafe9c3c4b6cb503ae02719c63864cd38dc
Bendixx <Bendik.Berntsen@nav.no>
2024-11-04 12:43:30 +0100

    changed .replace("\n", "") to .replace("\n", " ")

--- src/components/content/chat/chatbubbles/BobAnswerCitations.tsx
+++ src/components/content/chat/chatbubbles/BobAnswerCitations.tsx
@@ -23,7 +23,8 @@ function BobAnswerCitations({ citation, context }: BobAnswerCitationProps) {
         contextMetadata.KnowledgeArticleId === citation.article,
     )
 
-  const citeWords = citation.text.replace("\n", "").split(" ")
+  const citeWords = citation.text.replace("\n", " ").split(" ")
+
   const numWords = Math.min(citeWords.length / 2, 6)
   const textStart = citeWords.slice(0, numWords).join(" ")
   const textEnd = citeWords.slice(numWords).join(" ")
