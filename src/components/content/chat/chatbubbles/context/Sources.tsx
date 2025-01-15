e678a8ac2437ff46eb6c66cb8994bec9c7d8bf06
Bendixx <Bendik.Berntsen@nav.no>
2025-01-15 10:35:59 +0100

    Changed text for the read more ingress.

--- src/components/content/chat/chatbubbles/context/Sources.tsx
+++ src/components/content/chat/chatbubbles/context/Sources.tsx
@@ -1,54 +1,67 @@
-import { BodyShort, Heading, Label, Link, ReadMore, VStack } from "@navikt/ds-react"
-import { Context } from "../../../../../types/Message"
 import { ExternalLinkIcon } from "@navikt/aksel-icons"
+import {
+  BodyShort,
+  Heading,
+  Label,
+  Link,
+  ReadMore,
+  VStack,
+} from "@navikt/ds-react"
+import { Context } from "../../../../../types/Message"
 
 export const Sources = ({ context }: { context: Context[] }) => {
   const nksContext = context.filter(({ source }) => source === "nks")
   const navContext = context.filter(({ source }) => source === "navno")
 
   return (
-    <ReadMore header="Kilder" className="mt-4">
-      <VStack className="py-2">
+    <ReadMore header='Kilder' className='mt-4'>
+      <VStack className='py-2'>
         <BodyShort>
-          Denne oversikten viser alle kildene Bob har sett igjennom for å lage svaret.
+          Denne oversikten viser kildene Bob har brukt, men siden han bare leser
+          deler av dem, kan det være vanskelig å finne den eksakte informasjonen
+          som støtter svaret.
         </BodyShort>
-        <VStack gap="4">
-          <VStack gap="3">
-            <Heading level="2" size="xsmall">
+        <VStack gap='4'>
+          <VStack gap='3'>
+            <Heading level='2' size='xsmall'>
               Kunnskapsbasen
             </Heading>
-            {nksContext.map((ctx) => <NksSource context={ctx} />)}
+            {nksContext.map((ctx) => (
+              <NksSource context={ctx} />
+            ))}
           </VStack>
-          <VStack gap="3">
-            <Heading level="2" size="xsmall">
+          <VStack gap='3'>
+            <Heading level='2' size='xsmall'>
               Nav.no
             </Heading>
-            {navContext.map((ctx) => <NavSource context={ctx} />)}
+            {navContext.map((ctx) => (
+              <NavSource context={ctx} />
+            ))}
           </VStack>
         </VStack>
       </VStack>
-    </ReadMore >
+    </ReadMore>
   )
 }
 
 const NksSource = ({ context }: { context: Context }) => {
   return (
-    <Label size="small">
-      <Link href={`${context.url}#${context.anchor}`} target="_blank">
+    <Label size='small'>
+      <Link href={`${context.url}#${context.anchor}`} target='_blank'>
         {context.title}
         <ExternalLinkIcon title='Åpne artikkelen i ny fane' />
       </Link>
-    </Label >
+    </Label>
   )
 }
 
 const NavSource = ({ context }: { context: Context }) => {
   return (
-    <Label size="small">
-      <Link href={`${context.url}#${context.anchor}`} target="_blank">
+    <Label size='small'>
+      <Link href={`${context.url}#${context.anchor}`} target='_blank'>
         {context.title} - {context.anchor}
-        < ExternalLinkIcon title='Åpne artikkelen i ny fane' />
-      </Link >
-    </Label >
+        <ExternalLinkIcon title='Åpne artikkelen i ny fane' />
+      </Link>
+    </Label>
   )
 }
