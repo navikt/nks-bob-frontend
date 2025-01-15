51c7b49b526714148039e65171dc0920ca951be7
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2025-01-15 10:07:34 +0100

    add context/sources component

---
+++ src/components/content/chat/chatbubbles/context/Sources.tsx
@@ -0,0 +1,54 @@
+import { BodyShort, Heading, Label, Link, ReadMore, VStack } from "@navikt/ds-react"
+import { Context } from "../../../../../types/Message"
+import { ExternalLinkIcon } from "@navikt/aksel-icons"
+
+export const Sources = ({ context }: { context: Context[] }) => {
+  const nksContext = context.filter(({ source }) => source === "nks")
+  const navContext = context.filter(({ source }) => source === "navno")
+
+  return (
+    <ReadMore header="Kilder" className="mt-4">
+      <VStack className="py-2">
+        <BodyShort>
+          Denne oversikten viser alle kildene Bob har sett igjennom for å lage svaret.
+        </BodyShort>
+        <VStack gap="4">
+          <VStack gap="3">
+            <Heading level="2" size="xsmall">
+              Kunnskapsbasen
+            </Heading>
+            {nksContext.map((ctx) => <NksSource context={ctx} />)}
+          </VStack>
+          <VStack gap="3">
+            <Heading level="2" size="xsmall">
+              Nav.no
+            </Heading>
+            {navContext.map((ctx) => <NavSource context={ctx} />)}
+          </VStack>
+        </VStack>
+      </VStack>
+    </ReadMore >
+  )
+}
+
+const NksSource = ({ context }: { context: Context }) => {
+  return (
+    <Label size="small">
+      <Link href={`${context.url}#${context.anchor}`} target="_blank">
+        {context.title}
+        <ExternalLinkIcon title='Åpne artikkelen i ny fane' />
+      </Link>
+    </Label >
+  )
+}
+
+const NavSource = ({ context }: { context: Context }) => {
+  return (
+    <Label size="small">
+      <Link href={`${context.url}#${context.anchor}`} target="_blank">
+        {context.title} - {context.anchor}
+        < ExternalLinkIcon title='Åpne artikkelen i ny fane' />
+      </Link >
+    </Label >
+  )
+}
