48f65b91467d8f0659f300a736340cd6f92030e9
Bendixx <Bendik.Berntsen@nav.no>
2025-01-30 11:50:07 +0100

    Styling changes for the "edit question" button

--- src/components/content/chat/chatbubbles/UserQuestionBubble.tsx
+++ src/components/content/chat/chatbubbles/UserQuestionBubble.tsx
@@ -1,53 +1,57 @@
 import { Message } from "../../../../types/Message.ts"
 
+import { PencilWritingIcon } from "@navikt/aksel-icons"
+import { BodyLong, Button, Tooltip } from "@navikt/ds-react"
 import { memo } from "react"
-import { BodyLong, Button } from "@navikt/ds-react"
 import Markdown from "react-markdown"
 import rehypeRaw from "rehype-raw"
 import "./ChatBubbles.css"
-import { PencilIcon } from "@navikt/aksel-icons"
 
 interface UserChatBubbleProps {
   userQuestion?: Message
   setInputValue: React.Dispatch<React.SetStateAction<string>>
 }
 
-const UserQuestionBubble = memo(({ userQuestion, setInputValue }: UserChatBubbleProps) => {
-  const question = userQuestion?.content.replace(/\n/g, "<br>")
+const UserQuestionBubble = memo(
+  ({ userQuestion, setInputValue }: UserChatBubbleProps) => {
+    const question = userQuestion?.content.replace(/\n/g, "<br>")
 
-  const editQuestion = () => {
-    if (userQuestion) {
-      setInputValue(userQuestion.content)
+    const editQuestion = () => {
+      if (userQuestion) {
+        setInputValue(userQuestion.content)
+      }
     }
-  }
 
-  return (
-    <div className="self-end w-fit flex flex-row justify-end gap-2 mb-[20px]">
-      <div className="flex flex-col justify-center align-center">
-        <Button
-          variant="tertiary-neutral"
-          title="Rediger spørsmål"
-          onClick={editQuestion}
-        >
-          <PencilIcon />
-        </Button>
+    return (
+      <div className='mb-[20px] flex w-fit flex-row items-end gap-2 self-end'>
+        <div className='questionbubble max-w-prose'>
+          <BodyLong>
+            <Markdown rehypePlugins={[rehypeRaw]}>{question}</Markdown>
+          </BodyLong>
+        </div>
+        <Tooltip content='Rediger spørsmålet' placement='right'>
+          <Button
+            variant='tertiary-neutral'
+            size='small'
+            aria-label='Rediger spørsmålet'
+            onClick={editQuestion}
+            icon={<PencilWritingIcon />}
+            className='mb-[-0.45rem]'
+          />
+        </Tooltip>
       </div>
-      <div className='questionbubble max-w-prose'>
-        <BodyLong>
-          <Markdown rehypePlugins={[rehypeRaw]}>{question}</Markdown>
-        </BodyLong>
-      </div>
-    </div>
-  )
-}, (prevProps, nextProps) => {
-  const prevMessage = prevProps.userQuestion
-  const nextMessage = nextProps.userQuestion
+    )
+  },
+  (prevProps, nextProps) => {
+    const prevMessage = prevProps.userQuestion
+    const nextMessage = nextProps.userQuestion
 
-  if (!prevMessage?.pending) {
-    return true
-  }
+    if (!prevMessage?.pending) {
+      return true
+    }
 
-  return prevMessage === nextMessage
-})
+    return prevMessage === nextMessage
+  },
+)
 
 export default UserQuestionBubble
