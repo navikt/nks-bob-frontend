e422ffbb8cfedeeecb8c7041221a938eadd29780
Bendixx <Bendik.Berntsen@nav.no>
2025-02-17 09:47:41 +0100

    changes to followupquestions

--- src/components/content/followupquestions/FollowUpQuestions.tsx
+++ src/components/content/followupquestions/FollowUpQuestions.tsx
@@ -1,38 +1,35 @@
-import { Label } from "@navikt/ds-react"
-import { memo, useRef } from "react"
-import { useInputFieldContext } from "../../inputfield/InputField.tsx"
+import { BodyShort, Label } from "@navikt/ds-react"
+import { memo } from "react"
 import "./FollowUpQuestions.css"
 
 interface FollowUpQuestionsProps {
   followUp: string[]
+  onSend: (question: string) => void
   className?: string
 }
 
 export const FollowUpQuestions = memo(
-  ({ followUp, className }: FollowUpQuestionsProps) => {
-    const { inputValue, setInputValue, focusTextarea } = useInputFieldContext()
-    const originalInputValueRef = useRef(inputValue)
-
-    const handleClick = (question: string) => {
-      setInputValue(question)
-      originalInputValueRef.current = question
-      focusTextarea()
-    }
-
+  ({ followUp, onSend, className }: FollowUpQuestionsProps) => {
     return (
       followUp.length > 0 && (
-        <div className={`flex flex-col gap-2 overflow-auto ${className}`}>
+        <div
+          className={`flex flex-col gap-2 overflow-hidden pt-2 ${className}`}
+        >
           <Label size='small'>Forslag fra Bob</Label>
-          <div className='flex flex-row gap-2'>
+          <div className='questionscontainer relative flex flex-col gap-2'>
             {followUp.map((question, index) => (
               <button
-                onClick={() => handleClick(question)}
+                onClick={() => onSend(question)}
                 key={`question-${index}`}
-                className='followup-button basis-1/3 truncate transition-all'
+                className={`followupchip truncate transition-all question-${index}`}
               >
-                <span className='navds-body-short--small truncate'>
+                <BodyShort
+                  size='small'
+                  align='start'
+                  className='question-text truncate'
+                >
                   {question}
-                </span>
+                </BodyShort>
               </button>
             ))}
           </div>
