80b04ce49fe1dd8e288dc623ca40599a49a5e7a1
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2025-04-29 11:10:32 +0200

    add website id for prod

--- src/utils/AnalyticsProvider.tsx
+++ src/utils/AnalyticsProvider.tsx
@@ -1,6 +1,8 @@
 const isProd = window.location.hostname === "bob.ansatt.nav.no"
 
-const websiteId = isProd ? "" : "7a73382f-ec5b-4c80-b3f2-154388c32234"
+const websiteId = isProd
+  ? "7d7d1d3b-45ce-4e1f-a23a-aabc7c2db1f5"
+  : "7a73382f-ec5b-4c80-b3f2-154388c32234"
 
 const domains = isProd ? "bob.ansatt.nav.no" : "bob.ansatt.dev.nav.no"
 
