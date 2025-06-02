0232fe8916494d98da323956464f9577f61199de
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2025-06-02 13:15:49 +0200

    textfield -> textarea when creating feedback

--- src/components/content/chat/feedback/GiveUsFeedback.tsx
+++ src/components/content/chat/feedback/GiveUsFeedback.tsx
@@ -4,78 +4,12 @@ import {
   Checkbox,
   CheckboxGroup,
   Modal,
-  TextField,
+  Textarea,
   Tooltip,
 } from "@navikt/ds-react"
 import { useMemo, useRef, useState } from "react"
 import { useAddFeedback } from "../../../../api/api.ts"
 import { Message } from "../../../../types/Message.ts"
-
-// interface GiveUsFeedbackProps {
-//   message: Message
-// }
-//
-// export const GiveUsFeedback = ({ message }: GiveUsFeedbackProps) => {
-//   const ref = useRef<HTMLDialogElement>(null)
-//
-//   function encodedText(text: string) {
-//     return encodeURIComponent(text).replace(
-//       /[-!'()*#]/g,
-//       (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`,
-//     )
-//   }
-//
-//   function handleButtonClick() {
-//     amplitude.feilMeldt(message.id)
-//     window.open(
-//       `https://forms.office.com/Pages/ResponsePage.aspx?id=NGU2YsMeYkmIaZtVNSedC0RMRE_b5tVChsvEVOu-VahUMklHSElUVkE1Q0Q3S1RVSFNURlRERzRDNSQlQCN0PWcu&rc84a5384f81a40a8af1ad89105ae94c7=${encodedText(message.id)}&rc60f18cbf19d4053aed8e232647745b4=${encodedText(message.content)}`,
-//       "_blank",
-//     )
-//   }
-//
-//   return (
-//     <div>
-//       <Tooltip content='Meld inn feil svar'>
-//         <Button
-//           variant='tertiary-neutral'
-//           size='small'
-//           aria-label='Kopier svaret'
-//           icon={<ChatExclamationmarkIcon />}
-//           onClick={handleButtonClick}
-//         />
-//       </Tooltip>
-//       <Modal
-//         ref={ref}
-//         closeOnBackdropClick={true}
-//         aria-label='Gi oss tilbakemelding'
-//         header={{ closeButton: true, heading: "" }}
-//       >
-//         <Modal.Body>
-//           <div className='my-4'>
-//             <Heading size='xsmall' spacing>
-//               Svaret det gjelder
-//             </Heading>
-//             <div className='max-h-24 overflow-auto'>
-//               <BodyLong>
-//                 <Markdown className='markdown'>{message.content}</Markdown>
-//               </BodyLong>
-//             </div>
-//           </div>
-//           <div className='flex grow justify-start'>
-//             <Button
-//               size='small'
-//               variant='tertiary-neutral'
-//               onClick={() => ref.current?.close()}
-//               icon={<ArrowLeftIcon />}
-//             >
-//               Tilbake
-//             </Button>
-//           </div>
-//         </Modal.Body>
-//       </Modal>
-//     </div>
-//   )
-// }
 
 const OPTIONS = {
   "feil-med-svar": "Hele-/deler av svaret er feil",
@@ -156,9 +90,11 @@ export const FeedbackOnAnswer = ({ message }: FeedbackOnAnswerProps) => {
               </Checkbox>
             ))}
             {isAnnet ? (
-              <TextField
+              <Textarea
                 label='Gi oss en kort beskrivelse av hva som er galt'
                 onChange={(e) => setComment(e.target.value)}
+                minRows={1}
+                maxRows={4}
               />
             ) : null}
           </CheckboxGroup>
