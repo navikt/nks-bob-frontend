844d393e5176a62edd86fb2b5c9cdc79b272d216
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2026-02-03 13:46:37 +0100

    add option to select system theme

--- src/components/menu/darkmode/DarkModeToggle.tsx
+++ src/components/menu/darkmode/DarkModeToggle.tsx
@@ -1,22 +1,35 @@
-import { ThemeIcon } from "@navikt/aksel-icons"
-import { Button, Theme, Tooltip } from "@navikt/ds-react"
+import { CheckmarkIcon, MonitorIcon, MoonIcon, SunIcon, ThemeIcon } from "@navikt/aksel-icons"
+import { ActionMenu, Button, HStack, Theme, Tooltip } from "@navikt/ds-react"
 import { useHotkeys } from "react-hotkeys-hook"
 import { create } from "zustand"
 import { createJSONStorage, persist } from "zustand/middleware"
 import analytics from "../../../utils/analytics"
+import { useEffect, useState } from "react"
+
+type ThemeType = "light" | "dark" | "system"
+
+const themeTypeNorsk = (theme: ThemeType): "lys" | "mørk" | "system" => {
+  if (theme === "light") {
+    return "lys"
+  }
+  if (theme === "dark") {
+    return "mørk"
+  }
+  return "system"
+}
 
 type ThemeState = {
-  currentTheme: "light" | "dark"
-  setTheme: (theme: "light" | "dark") => void
+  currentTheme: ThemeType
+  setTheme: (theme: ThemeType) => void
   toggleTheme: () => void
 }
 
 export const useTheme = create<ThemeState>()(
   persist(
     (set) => {
-      const setTheme = (theme: "light" | "dark") =>
+      const setTheme = (theme: ThemeType) =>
         set((state) => {
-          analytics.mørkModusByttet(theme === "dark" ? "mørk" : "lys")
+          analytics.mørkModusByttet(themeTypeNorsk(theme))
           return {
             ...state,
             currentTheme: theme,
@@ -56,27 +69,114 @@ export const useTheme = create<ThemeState>()(
 )
 
 export const ThemeButton = () => {
-  const { currentTheme, toggleTheme } = useTheme()
-
-  const tooltip = currentTheme === "dark" ? "Endre til lys modus ( Alt+Ctrl+D )" : "Endre til mørk modus ( Alt+Ctrl+D )"
+  const { currentTheme, toggleTheme, setTheme } = useTheme()
 
   useHotkeys("alt+ctrl+d", () => toggleTheme(), {
     enableOnFormTags: true,
   })
 
   return (
-    <Tooltip content={tooltip}>
-      <Button
-        variant='tertiary'
-        data-color='neutral'
-        icon={<ThemeIcon aria-hidden />}
-        onClick={() => toggleTheme()}
-      />
-    </Tooltip>
+    <ActionMenu>
+      <Tooltip content='Endre fargetema ( Alt+Ctrl+D )'>
+        <ActionMenu.Trigger>
+          <Button
+            variant='tertiary'
+            data-color='neutral'
+            icon={<ThemeIcon aria-hidden />}
+          />
+        </ActionMenu.Trigger>
+      </Tooltip>
+      <ActionMenu.Content>
+        <ActionMenu.Label>Velg fargetema</ActionMenu.Label>
+        <ActionMenu.Group aria-label='Velg fargetema'>
+          <ActionMenu.Item
+            icon={<SunIcon />}
+            aria-current={currentTheme === "light"}
+            onSelect={() => setTheme("light")}
+          >
+            <HStack
+              gap='space-24'
+              align='center'
+            >
+              Lyst
+              {currentTheme === "light" && (
+                <CheckmarkIcon
+                  aria-hidden
+                  fontSize='1.25rem'
+                />
+              )}
+            </HStack>
+          </ActionMenu.Item>
+          <ActionMenu.Item
+            icon={<MoonIcon />}
+            aria-current={currentTheme === "dark"}
+            onSelect={() => setTheme("dark")}
+          >
+            <HStack
+              gap='space-24'
+              align='center'
+            >
+              Mørkt
+              {currentTheme === "dark" && (
+                <CheckmarkIcon
+                  aria-hidden
+                  fontSize='1.25rem'
+                />
+              )}
+            </HStack>
+          </ActionMenu.Item>
+          <ActionMenu.Item
+            icon={<MonitorIcon />}
+            aria-current={currentTheme === "system"}
+            onSelect={() => setTheme("system")}
+          >
+            <HStack
+              gap='space-24'
+              align='center'
+            >
+              System
+              {currentTheme === "system" && (
+                <CheckmarkIcon
+                  aria-hidden
+                  fontSize='1.25rem'
+                />
+              )}
+            </HStack>
+          </ActionMenu.Item>
+        </ActionMenu.Group>
+      </ActionMenu.Content>
+    </ActionMenu>
   )
 }
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const { currentTheme } = useTheme()
-  return <Theme theme={currentTheme}>{children}</Theme>
+
+  const resolveTheme = (): "light" | "dark" => {
+    if (currentTheme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+      return systemTheme
+    }
+    return currentTheme
+  }
+
+  const [theme, setTheme] = useState<"light" | "dark">(resolveTheme)
+
+  const colorSchemeEventListener = (e: MediaQueryListEvent) => {
+    if (currentTheme === "system") {
+      setTheme(e.matches ? "dark" : "light")
+    }
+  }
+
+  useEffect(() => {
+    if (currentTheme !== theme) {
+      setTheme(resolveTheme())
+    }
+
+    const prefersColorScheme = window.matchMedia("(prefers-color-scheme: dark)")
+    prefersColorScheme.addEventListener("change", colorSchemeEventListener)
+    return () => prefersColorScheme.removeEventListener("change", colorSchemeEventListener)
+  }, [currentTheme])
+
+  return <Theme theme={theme}>{children}</Theme>
 }
