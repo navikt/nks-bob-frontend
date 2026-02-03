5244d09accab36db08959c45d6da25415a67e789
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2026-02-03 10:40:26 +0100

    adjust user question bubble button sizes

--- src/components/content/chat/chatbubbles/UserQuestionBubble.tsx
+++ src/components/content/chat/chatbubbles/UserQuestionBubble.tsx
@@ -40,7 +40,12 @@ const UserQuestionBubble = memo(
               copyText={raw}
               size='small'
               activeText='Kopiert!'
-              icon={<FilesIcon aria-hidden />}
+              icon={
+                <FilesIcon
+                  aria-hidden
+                  fontSize='1.25rem'
+                />
+              }
             />
           </Tooltip>
           <Tooltip
@@ -53,7 +58,12 @@ const UserQuestionBubble = memo(
               size='small'
               aria-label='Rediger spørsmålet'
               onClick={editQuestion}
-              icon={<PencilWritingIcon />}
+              icon={
+                <PencilWritingIcon
+                  aria-hidden
+                  fontSize='1.25rem'
+                />
+              }
             />
           </Tooltip>
         </div>
