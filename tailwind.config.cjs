87bf69154b962b72c71b97b6d47296abd92f4271
Bendixx <Bendik.Berntsen@nav.no>
2026-02-16 22:24:15 +0100

    small styling changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
       screens: {
         tallWide: { raw: "(max-width: 800px) and (min-height: 1200px)" },
         marginWide: { raw: "(min-width: 800px)" },
-        narrowWidth: { raw: "(max-width: 750px)" },
+        narrowWidth: { raw: "(max-width: 750px) and (min-height: 1120px)" },
         narrowHeight: { raw: "(max-height: 1000px)" }
       },
     },
