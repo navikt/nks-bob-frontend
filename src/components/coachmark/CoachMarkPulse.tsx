1f123db87b8cf6de45ee16371a5c397877daaa42
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2026-02-17 14:29:46 +0100

    remove coach-mark code

--- src/components/coachmark/CoachMarkPulse.tsx
+++
@@ -1,3 +0,0 @@
-export const CoachMarkPulse = () => {
-  return <div></div>
-}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,5 @@
 import { Link } from "react-router"
-import { useUserConfig } from "../../api/api.ts"
-import { CoachMark } from "../coachmark/CoachMark.tsx"
-import { MainButtonsExplanation } from "../coachmark/CoachmarkContent.tsx"
 import Guide from "../content/guide/Guide.tsx"
-import { NewConceptMessage } from "../content/guide/GuideModals.tsx"
 import { NewButton } from "../menu/NewButton.tsx"
 import { ThemeButton } from "../menu/darkmode/DarkModeToggle.tsx"
 import { NotificationToggle } from "../notification/NotificationDrawer.tsx"
@@ -15,9 +11,6 @@ interface HeaderProps {
 }
 
 function Header({ conversation }: HeaderProps) {
-  const { userConfig } = useUserConfig()
-  const coachMarkKey = "coachMarkShownHeader"
-
   return (
     <div
       className='tallWide:sticky tallWide:top-0 tallWide:left-0 tallWide:right-0 tallWide:z-50 bg-ax-bg-default mb-1 w-full'
@@ -35,22 +28,10 @@ function Header({ conversation }: HeaderProps) {
         </div>
         <div className='flex h-full max-h-30 gap-3'>
           <div className='flex items-center justify-center align-middle'>
-            {conversation && (
-              <>
-                <CoachMark
-                  title='Her finner du overordnede valg'
-                  buttonText='Den er grei!'
-                  coachMarkKey={coachMarkKey}
-                >
-                  <MainButtonsExplanation />
-                </CoachMark>
-                <NewButton conversationId={conversation} />
-              </>
-            )}
+            {conversation && <NewButton conversationId={conversation} />}
             <RegretNewButton />
             <NotificationToggle />
             <Guide />
-            {userConfig?.showNewConceptInfo && <NewConceptMessage />}
             <ThemeButton />
           </div>
         </div>
