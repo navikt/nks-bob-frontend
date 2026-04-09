8fa1fd3242139a64aa84b37015b27c90f6fd44b4
Bendixx <Bendik.Berntsen@nav.no>
2026-04-09 10:29:41 +0200

    changed background color to default, new logo and new background

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router"
+import { BobLogo } from "../../assets/icons/logo/BobLogo.tsx"
 import Guide from "../content/guide/Guide.tsx"
 import { NewButton } from "../menu/NewButton.tsx"
 import { ThemeButton } from "../menu/darkmode/DarkModeToggle.tsx"
 import { NotificationToggle } from "../notification/NotificationDrawer.tsx"
-import { NAVLogo } from "./nav-logo.tsx"
 import RegretNewButton from "./regretbutton/RegretNewConversationButton.tsx"
 
 interface HeaderProps {
@@ -13,17 +13,17 @@ interface HeaderProps {
 function Header({ conversation }: HeaderProps) {
   return (
     <div
-      className='tallWide:sticky tallWide:top-0 tallWide:left-0 tallWide:right-0 tallWide:z-50 bg-ax-brand-magenta-100 dark:bg-ax-bg-default mb-1 w-full'
+      className='tallWide:sticky tallWide:top-0 tallWide:left-0 tallWide:right-0 tallWide:z-50 bg-ax-bg-default mb-1 w-full'
       style={{ viewTransitionName: "nav-header" }}
     >
       <div className='marginWide:px-16 mx-auto flex max-h-16 w-full max-w-screen-2xl items-center justify-between self-center px-4 py-4'>
-        <div className='ml-2 flex max-w-24'>
+        <div className='ml-1 flex max-w-16'>
           <Link
             to='https://www.nav.no/'
             target='_blank'
             tabIndex={-1}
           >
-            <NAVLogo />
+            <BobLogo />
           </Link>
         </div>
         <div className='flex h-full max-h-30 gap-3'>
